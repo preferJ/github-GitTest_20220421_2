3eeb6d14ef5e320f900e86ae557f0eeef87bb36c
preferJ <prefer0415@gmail.com>
2022-04-21 11:19:11 +0900

    second commit

--- src/test/Hi1.java
+++ src/test/Hi1.java
@@ -4,6 +4,7 @@ public class Hi1 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is Hi 1 ~");
+		System.out.println("This is new Hi 1 ~~~");
 	}
 
 }
--- src/test/Hi2.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Hi2 {
-	public static void main(String[] args) {
-		
-		System.out.println("This is Hi 1 ~");
-	}
-
-}
